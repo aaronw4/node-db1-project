6ff8c07eefd88753e9ab6ea7e43eeec29d298baf
Aaron Williams <aaronrw4@yahoo.com>
2020-01-29 19:11:47 -0800

    Installed nodemon, get accounts works

--- accounts-router.js
+++ accounts-router.js
@@ -4,5 +4,14 @@ const db = require('./data/dbConfig');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const accounts = await db('accounts');
+        res.status(200).json(accounts);
+    } catch (err) {
+        res.status(500).json({error: 'Failed to get accounts.'})
+    }
+})
+
 
 module.exports = router;
